924187978d5ee8a5768fef18f2092a02b84e8c6a
nate <nate@intrinsicautomations.com>
2018-08-07 03:50:42 +0000

    updated script to remove heading characteristic

--- SampleScripts/Boogio6/StreamingExample.py
+++ SampleScripts/Boogio6/StreamingExample.py
@@ -157,7 +157,6 @@ if __name__ == "__main__":
 forceCharacteristicHandle = None
 accelerationCharacteristicHandle = None
 rotationCharacteristicHandle = None
-headingCharacteristicHandle = None
 
 
 
@@ -176,7 +175,6 @@ class MyDelegate(DefaultDelegate):
         
         self.ACCELERATION_CONVERSION_COEFFICIENT = 1.0 / 1000.0
         self.ROTATION_CONVERSION_COEFFICIENT     = 1.0 / 1000.0
-        self.HEADING_CONVERSION_COEFFICIENT      = 1.0 / 1000.0
 
         self.forceToe = 0.00
         self.forceBall = 0.00
@@ -190,12 +188,7 @@ class MyDelegate(DefaultDelegate):
         self.rotationX = 0.000
         self.rotationY = 0.000
         self.rotationZ = 0.000
-
-        self.headingQW = 0.000
-        self.headingQX = 0.000
-        self.headingQY = 0.000
-        self.headingQZ = 0.000
-        
+        self.rotationW = 0.000
 
     def handleNotification(self, hnd, data):
 
@@ -230,6 +223,7 @@ class MyDelegate(DefaultDelegate):
             self.rotationX = struct.unpack('<H', data[0:2])[0]
             self.rotationY = struct.unpack('<H', data[2:4])[0]
             self.rotationZ = struct.unpack('<H', data[4:6])[0]
+            self.rotationW = struct.unpack('<H', data[6:8])[0]
 
             #2's complement
             if(self.rotationX > self.HALF_OF_MAX_SHORT_VALUE):
@@ -238,33 +232,14 @@ class MyDelegate(DefaultDelegate):
                self.rotationY = self.rotationY - self.MAX_SHORT_VALUE
             if(self.rotationZ > self.HALF_OF_MAX_SHORT_VALUE):
                self.rotationZ = self.rotationZ - self.MAX_SHORT_VALUE
+            if(self.rotationW > self.HALF_OF_MAX_SHORT_VALUE):
+               self.rotationW = self.rotationW - self.MAX_SHORT_VALUE
 
             self.rotationX *= self.ROTATION_CONVERSION_COEFFICIENT
             self.rotationY *= self.ROTATION_CONVERSION_COEFFICIENT
             self.rotationZ *= self.ROTATION_CONVERSION_COEFFICIENT
+            self.rotationW *= self.ROTATION_CONVERSION_COEFFICIENT
 
-        elif (hnd == headingCharacteristicHandle):
-            self.headingQW = struct.unpack('<H', data[0:2])[0]
-            self.headingQX = struct.unpack('<H', data[2:4])[0]
-            self.headingQY = struct.unpack('<H', data[4:6])[0]
-            self.headingQZ = struct.unpack('<H', data[6:8])[0]
-
-            #2's complement
-            if(self.headingQW > self.HALF_OF_MAX_SHORT_VALUE):
-               self.headingQW = self.headingQW - self.MAX_SHORT_VALUE
-            if(self.headingQX > self.HALF_OF_MAX_SHORT_VALUE):
-               self.headingQX = self.headingQX - self.MAX_SHORT_VALUE
-            if(self.headingQY > self.HALF_OF_MAX_SHORT_VALUE):
-               self.headingQY = self.headingQY - self.MAX_SHORT_VALUE
-            if(self.headingQZ > self.HALF_OF_MAX_SHORT_VALUE):
-               self.headingQZ = self.headingQZ - self.MAX_SHORT_VALUE
-            
-            self.headingQW *= self.HEADING_CONVERSION_COEFFICIENT
-            self.headingQX *= self.HEADING_CONVERSION_COEFFICIENT
-            self.headingQY *= self.HEADING_CONVERSION_COEFFICIENT
-            self.headingQZ *= self.HEADING_CONVERSION_COEFFICIENT
-
-            #print(self.headingX, self.headingY, self.headingZ)
         else:
             teptep = binascii.b2a_hex(data)
             print('Notification: UNKOWN: hnd {}, data {}'.format(hnd, teptep))
@@ -289,7 +264,6 @@ boogioShoeSensorService = None
 forceCharacteristic = None
 accelerationCharacteristic = None
 rotationCharacteristic = None
-headingCharacteristic = None
 
 
 CCCD_UUID = 0x2902
@@ -317,11 +291,7 @@ for svc in boogioPeripheral.services:
                 rotationCharacteristicHandle = characteristic.getHandle()
                 rotationCCCD = characteristic.getDescriptors(forUUID=CCCD_UUID)[0]
                 rotationCCCD.write(b"\x01\x00", True)
-            elif characteristic.uuid == "f3641404-00B0-4240-ba50-05ca45bf8abc":
-                headingCharacteristic = characteristic
-                headingCharacteristicHandle = characteristic.getHandle()
-                headingCCCD = characteristic.getDescriptors(forUUID=CCCD_UUID)[0]
-                headingCCCD.write(b"\x01\x00", True)
+            
 
 
 
@@ -372,7 +342,6 @@ while not shouldQuit:
     
     labelSurface = metricsFont.render("Acceleration ", 1, (255,255,255))
     DISPLAYSURF.blit(labelSurface, (cursorX, vSpacing * 3))
-
     
     labelSurface = metricsFont.render("[Gravities]:", 1, (255,255,255))
     DISPLAYSURF.blit(labelSurface, (cursorX, vSpacing * 4))
@@ -380,20 +349,14 @@ while not shouldQuit:
     labelSurface = metricsFont.render("Rotation ", 1, (255,255,255))
     DISPLAYSURF.blit(labelSurface, (cursorX, vSpacing * 6))
 
-    labelSurface = metricsFont.render("[degrees/sec]:", 1, (255,255,255))
+    labelSurface = metricsFont.render("[quaternion]:", 1, (255,255,255))
     DISPLAYSURF.blit(labelSurface, (cursorX, vSpacing * 7))
 
-    labelSurface = metricsFont.render("Heading ", 1, (255,255,255))
+    labelSurface = metricsFont.render("Force ", 1, (255,255,255))
     DISPLAYSURF.blit(labelSurface, (cursorX, vSpacing * 9))
 
-    labelSurface = metricsFont.render("[quaternion]:", 1, (255,255,255))
-    DISPLAYSURF.blit(labelSurface, (cursorX, vSpacing * 10))
-
-    labelSurface = metricsFont.render("Force ", 1, (255,255,255))
-    DISPLAYSURF.blit(labelSurface, (cursorX, vSpacing * 12))
-
     labelSurface = metricsFont.render("[Newtons]:", 1, (255,255,255))
-    DISPLAYSURF.blit(labelSurface, (cursorX, vSpacing * 13))
+    DISPLAYSURF.blit(labelSurface, (cursorX, vSpacing * 10))
 
     
     # readings
@@ -445,62 +408,48 @@ while not shouldQuit:
     rotationZ = str(round(boogioDelegate.rotationZ, 2))
     labelSurface = metricsFont.render(rotationZ, 1, BLUE)
     DISPLAYSURF.blit(labelSurface, (cursorX + hSpacing*24, vSpacing * 7))
-    
-    
 
-    headingQX = str(round(boogioDelegate.headingQX, 2))
-    labelSurface = metricsFont.render(headingQX, 1, RED)
-    DISPLAYSURF.blit(labelSurface, (cursorX + hSpacing*8, vSpacing * 10))
-
-    headingQY = str(round(boogioDelegate.headingQY, 2))
-    labelSurface = metricsFont.render(headingQY, 1, GREEN)
-    DISPLAYSURF.blit(labelSurface, (cursorX + hSpacing*16, vSpacing * 10))
-
-    headingQZ = str(round(boogioDelegate.headingQZ, 2))
-    labelSurface = metricsFont.render(headingQZ, 1, BLUE)
-    DISPLAYSURF.blit(labelSurface, (cursorX + hSpacing*24, vSpacing * 10))
-
-    headingQW = str(round(boogioDelegate.headingQW, 2))
-    labelSurface = metricsFont.render(headingQW, 1, YELLOW)
-    DISPLAYSURF.blit(labelSurface, (cursorX + hSpacing*32, vSpacing * 10))
+    rotationW = str(round(boogioDelegate.rotationW, 2))
+    labelSurface = metricsFont.render(rotationW, 1, YELLOW)
+    DISPLAYSURF.blit(labelSurface, (cursorX + hSpacing*32, vSpacing * 7))
     
     
     
     
 
     labelSurface = metricsFont.render("________________________________________________________", 1, (255,255,255))
-    DISPLAYSURF.blit(labelSurface, (cursorX, vSpacing * 12))
+    DISPLAYSURF.blit(labelSurface, (cursorX, vSpacing * 9))
 
 
     labelSurface = metricsFont.render("Toe", 1, ORANGE)
-    DISPLAYSURF.blit(labelSurface, (cursorX + hSpacing*8, vSpacing * 12))
+    DISPLAYSURF.blit(labelSurface, (cursorX + hSpacing*8, vSpacing * 9))
     
     labelSurface = metricsFont.render("Ball", 1, ORANGE)
-    DISPLAYSURF.blit(labelSurface, (cursorX + hSpacing*16, vSpacing * 12))
+    DISPLAYSURF.blit(labelSurface, (cursorX + hSpacing*16, vSpacing * 9))
 
     labelSurface = metricsFont.render("Arch", 1, ORANGE)
-    DISPLAYSURF.blit(labelSurface, (cursorX + hSpacing*24, vSpacing * 12))
+    DISPLAYSURF.blit(labelSurface, (cursorX + hSpacing*24, vSpacing * 9))
 
     labelSurface = metricsFont.render("Heel", 1, ORANGE)
-    DISPLAYSURF.blit(labelSurface, (cursorX + hSpacing*32, vSpacing * 12))
+    DISPLAYSURF.blit(labelSurface, (cursorX + hSpacing*32, vSpacing * 9))
     
     
 
     forceToe = str(round(boogioDelegate.forceToe, 2))
     labelSurface = metricsFont.render(forceToe, 1, ORANGE)
-    DISPLAYSURF.blit(labelSurface, (cursorX + hSpacing*8, vSpacing * 13))
+    DISPLAYSURF.blit(labelSurface, (cursorX + hSpacing*8, vSpacing * 10))
 
     forceBall = str(round(boogioDelegate.forceBall, 2))
     labelSurface = metricsFont.render(forceBall, 1, ORANGE)
-    DISPLAYSURF.blit(labelSurface, (cursorX + hSpacing*16, vSpacing * 13))
+    DISPLAYSURF.blit(labelSurface, (cursorX + hSpacing*16, vSpacing * 10))
 
     forceArch = str(round(boogioDelegate.forceArch, 2))
     labelSurface = metricsFont.render(forceArch, 1, ORANGE)
-    DISPLAYSURF.blit(labelSurface, (cursorX + hSpacing*24, vSpacing * 13))
+    DISPLAYSURF.blit(labelSurface, (cursorX + hSpacing*24, vSpacing * 10))
 
     forceHeel = str(round(boogioDelegate.forceHeel, 2))
     labelSurface = metricsFont.render(forceHeel, 1, ORANGE)
-    DISPLAYSURF.blit(labelSurface, (cursorX + hSpacing*32, vSpacing * 13))
+    DISPLAYSURF.blit(labelSurface, (cursorX + hSpacing*32, vSpacing * 10))
 
 
 
