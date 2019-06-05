a181d047649a5b4a557c56db2fe98def8444c9e6
IA-Nate <nate@intrinsicautomations.com>
2019-06-04 18:44:42 -0700

    made some scripts. not all of these will work simultaneously yet.

--- CopyToDesktop/transmit-both-devices.sh
+++ CopyToDesktop/transmit-both-devices.sh
@@ -2,5 +2,5 @@
 date
 echo Running streaming demo
 cd /home/pi/Dev/BoogioBaseStation/Python/
-sudo python3 streaming.py -b f5:47:18:cf:9c:dc
+sudo python3 streaming.py -p 21030 -e -b f5:47:18:cf:9c:dc
 echo done.
--- CopyToDesktop/transmit-device-a.sh
+++ CopyToDesktop/transmit-device-a.sh
@@ -2,5 +2,5 @@
 date
 echo Running streaming demo
 cd ~/Dev/BoogioBaseStation/Python/
-sudo python3 streaming.py -p 21030 -e -b dc:80:07:ef:8b:cf
+sudo python3 streaming.py -c -p 20131 -b dc:80:07:ef:8b:cf
 echo done.
